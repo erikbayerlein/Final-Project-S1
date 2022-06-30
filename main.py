64f0a7581011ec6c9e23f4a8a5c28b2e8051fae1
erikbayerlein <100382086+erikbayerlein@users.noreply.github.com>
2022-06-30 16:36:07 -0300

    Add files via upload
    Primeiro dia

---
+++ main.py
@@ -0,0 +1,77 @@
+def opcao_1 ():
+    quantidade = int(input("Quantas peças voce gostaria de inserir?"))
+    for i in range(quantidade):
+        arquivo_armario = open("armario.txt", "r")
+        conteudo = arquivo_armario.readline()
+        list_conteudo = conteudo.split()
+        arquivo_armario.close()
+
+        arquivo_id = open("idpecas.txt","r")
+        conteudo_id = arquivo_id.readline()
+        cont_id = int(conteudo_id)
+        cont_id += 1
+        cont2_id = str(cont_id)
+        arquivo_id.close()
+
+        arquivo_id = open("idpecas.txt","w")
+        arquivo_id.write(cont2_id)
+        arquivo_id.close()
+
+        list_conteudo.pop(0)
+
+        list_info = []
+        for j in range(7):
+            print("Digite a informação seguinte: ", list_conteudo[j])
+            info = input()
+            info2 = info + " "
+            list_info.append(info2)
+        id_str = str(cont_id) + " "
+        list_info.insert(0, id_str)
+
+        arquivo_armario = open("armario.txt","a")
+        arquivo_armario.write("\n" + "".join(list_info))
+        arquivo_armario.close()
+
+    return quantidade, info
+
+#MENU DE ESCOLHA (FUNCAO MENU)
+def menu_principal(titulos):
+    for i in range(7):
+        print(titulos[i])
+
+    while True:
+        opcao_escolhida = int(input("Digite um número correspondente a opção do menu: "))
+        if opcao_escolhida < 1 or opcao_escolhida > 7:
+            print("Entrada inválida. Digite um número válido.")
+        else:
+            break
+    return opcao_escolhida
+
+#VARIAVEIS
+ 
+titulos = ["1- Inserir nova peça","2- Inserir novo estilo","3- Remover peça",
+    "4- Remover estilo","5- Alterar peça","6- Procurar peça", "7- Finalizar programa"]
+
+opcao_escolhida = menu_principal(titulos)
+
+if opcao_escolhida == 1:
+    opcao_1()
+'''
+elif opçao_escolhida == 2:
+    #inserir novo estilo
+
+elif opçao_escolhida == 3:
+    #remover peca
+
+elif opçao_escolhida == 4:
+    #remover estilo
+
+elif opçao_escolhida == 5:
+    #alterar peca
+
+elif opçao_escolhida == 6:
+    #procurar peça
+
+elif opçao_escolhida == 7:
+    exit()
+    '''
